7b11bf7fd8bc9af78c9abdbc9d8d17fc6951e0fe
kailang.huang <591620279@qq.com>
2024-01-03 16:53:47 +0800

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+# setup(name='ImageComposite',
+#       version='1.0.6',
+#       # py_modules=["processor"],
+#       description="image composite",
+#       author="kailang",
+#       license="MIT",
+#       packages=["processor"],
+#       # packages=find_packages(),
+#       # package_dir={'': 'processor'},
+#       classifiers=[
+#           "Programming Language :: Python :: 3",
+#           "License :: OSI Approved :: MIT License",
+#           "Operating System :: OS Independent",
+#       ],
+#       # package_dir={'processor': 'lib'},
+#       python_requires='>=3.6',
+#       )
